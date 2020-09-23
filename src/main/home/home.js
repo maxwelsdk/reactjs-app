fead46195d03a65fd554bab57e36c39d563c5536
vitorArpia <vitornunes@arpiatecnologia.com.br>
2020-09-22 21:44:18 -0300

    folder's assets and understanding settings

---
+++ src/main/home/home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+
+    render() {
+        return(
+            <p>Click on an emoji to view the emoji short name.</p>
+        )
+    }
+}
